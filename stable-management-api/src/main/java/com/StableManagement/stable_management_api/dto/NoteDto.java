c5ef3a9069d4266f10e00050488bd3079bb1ba81
Szymon Kozlowski <szymonkozlowski1092@gmail.com>
2025-06-09 17:51:17 +0200

    Create NoteDto.java

---
+++ stable-management-api/src/main/java/com/StableManagement/stable_management_api/dto/NoteDto.java
@@ -0,0 +1,58 @@
+package com.StableManagement.stable_management_api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public class NoteDto {
+    private Long id;
+    @NotBlank
+    private String content;
+    @NotNull
+    private LocalDateTime createdDate;
+    @NotNull
+    private Long creatorId;
+    @NotNull
+    private Long horseId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Long getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Long creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Long getHorseId() {
+        return horseId;
+    }
+
+    public void setHorseId(Long horseId) {
+        this.horseId = horseId;
+    }
+}
